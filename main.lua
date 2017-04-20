fa6febe5877921c96b0080c409b3d1c16731c2d2
lost-RD <lost_RD@live.com>
2017-04-20 15:23:28 +0930

    optimisations: canvas, tuning new features: HUD some tidying up

--- main.lua
+++ main.lua
@@ -3,117 +3,108 @@ rgb_red = {}
 rgb_green = {}
 rgb_blue = {}
 
-rgb_red[0] = 0xff
-rgb_green[0] = 0xff
-rgb_blue[0] = 0xff
+function comma_value(n) -- credit http://richard.warburton.it
+	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+	return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
+end
 
-rgb_red[1] = 0xe4
-rgb_green[1] = 0xe4
-rgb_blue[1] = 0xe4
+function set_rgb(k, r, g, b)
+	rgb_red[k] = r
+	rgb_green[k] = g
+	rgb_blue[k] = b
+end
 
-rgb_red[2] = 0x88
-rgb_green[2] = 0x88
-rgb_blue[2] = 0x88
-
-rgb_red[3] = 0x22
-rgb_green[3] = 0x22
-rgb_blue[3] = 0x22
-
-rgb_red[4] = 0xff
-rgb_green[4] = 0xa7
-rgb_blue[4] = 0xd1
-
-rgb_red[5] = 0xe5
-rgb_green[5] = 0x00
-rgb_blue[5] = 0x00
-
-rgb_red[6] = 0xe5
-rgb_green[6] = 0x95
-rgb_blue[6] = 0x00
-
-rgb_red[7] = 0xa0
-rgb_green[7] = 0x6a
-rgb_blue[7] = 0x42
-
-rgb_red[8] = 0xe5
-rgb_green[8] = 0xd9
-rgb_blue[8] = 0x00
-
-rgb_red[9] = 0x94
-rgb_green[9] = 0xe0
-rgb_blue[9] = 0x44
-
-rgb_red[10] = 0x02
-rgb_green[10] = 0xbe
-rgb_blue[10] = 0x01
-
-rgb_red[11] = 0x00
-rgb_green[11] = 0xe5
-rgb_blue[11] = 0xf0
-
-rgb_red[12] = 0x00
-rgb_green[12] = 0x83
-rgb_blue[12] = 0xc7
-
-rgb_red[13] = 0x00
-rgb_green[13] = 0x00
-rgb_blue[13] = 0xea
-
-rgb_red[14] = 0xe0
-rgb_green[14] = 0x4a
-rgb_blue[14] = 0xff
-
-rgb_red[15] = 0x82
-rgb_green[15] = 0x00
-rgb_blue[15] = 0x80
+set_rgb(0, 0xff, 0xff, 0xff)
+set_rgb(1, 0xe4, 0xe4, 0xe4)
+set_rgb(2, 0x88, 0x88, 0x88)
+set_rgb(3, 0x22, 0x22, 0x22)
+set_rgb(4, 0xff, 0xa7, 0xd1)
+set_rgb(5, 0xe5, 0x00, 0x00)
+set_rgb(6, 0xe5, 0x95, 0x00)
+set_rgb(7, 0xa0, 0x6a, 0x42)
+set_rgb(8, 0xe5, 0xd9, 0x00)
+set_rgb(9, 0x94, 0xe0, 0x44)
+set_rgb(10, 0x02, 0xbe, 0x01)
+set_rgb(11, 0x00, 0xe5, 0xf0)
+set_rgb(12, 0x00, 0x83, 0xc7)
+set_rgb(13, 0x00, 0x00, 0xea)
+set_rgb(14, 0xe0, 0x4a, 0xff)
+set_rgb(15, 0x82, 0x00, 0x80)
 
 function love.load()
-	love.window.setMode(1000, 1000)
-	love.window.setTitle("/r/place")
-
-	-- store all pixels
-	pixels = {}
-	for i = 0, 999 do
-		pixels[i] = {}
-	end
-
+	-- create a canvas
+	canvas = love.graphics.newCanvas(1000, 1000)
+	--create a font
+	font = love.graphics.newFont(24)
+	love.graphics.setFont(font)
+	-- hide the cursor
+	love.mouse.setVisible(false)
 	-- open csv file. this should not have the original header, and be
 	-- sorted by increasing timestamp
 	-- cat tile_placements.csv | sort --field-separator=',' --key=1 > sorted.csv
 	fh = io.open("sorted.csv", "r")
+	-- initialise here for scope reasons
+	hud_line_1 = ""
+	hud_line_2 = ""
+	updates = 1000
+	lines_read = 0
+	lines_update = 0
 end
 
 function love.update(dt)
-	-- read a lot of pixels into our pixel array
-	-- this will be displayed in love.draw()
-	for i = 0, 1000000 do
-		-- read next line
-		local line = fh:read()
+	hud_line_1 = "status: "..(fh and "working" or "done").." | progress: "..math.floor(100*lines_read/16559898).."%"
+	hud_line_2 = "pixels per second: "..comma_value(math.floor(updates/dt))
+	if fh then
+		for i = 0,updates do
+			-- read next line
+			local line = fh:read()
+			-- handling the end of the file
+			if (line == nil) or (line == "ts,user,x_coordinate,y_coordinate,color") then
+				fh:close()
+				print("eof reached")
+				return
+			end
+			-- split the line into variables
+			local ts,user,x,y,c = line:match("(.+),(.+),(.+),(.+),(.+)")
+			-- conver strings to ints
+			x = tonumber(x)
+			y = tonumber(y)
+			c = tonumber(c)
 
-		if line == nil then return end
-
-		local ts,user,x,y,c = line:match("(.+),(.+),(.+),(.+),(.+)")
-
-		x = tonumber(x)
-		y = tonumber(y)
-		c = tonumber(c)
-
-		--print(x, y, c)
-
-		if x > 999 or y > 999 then break end
-		pixels[x][y] = c
+			--print(x, y, c)
+			-- should never fire but who knows
+			if x > 999 or y > 999 then break end
+			-- draw the pixel to the canvas
+			putPixel(x, y, c)
+			lines_read = lines_read + 1
+		end
 	end
 end
 
 function love.draw()
-	-- loop through pixels and draw them
-	for y = 0, 999 do
-		for x = 0, 999 do
-			local c = pixels[x][y]
-			if c then
-				putPixel(x, y, c)
-			end
-		end
+	-- important setup each frame
+	love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setBlendMode("alpha", "premultiplied")
+	-- draw the canvas to the screen
+	love.graphics.draw(canvas)
+	-- check if mouse is on screen
+	x, y = love.mouse.getPosition()
+	if (x > 0) and (x < 999) and (y > 0) and (y < 999) then
+		-- setup for HUD
+		love.graphics.setBlendMode("alpha")
+		-- draw HUD
+		-- line 1
+		love.graphics.setColor(0, 0, 0, 255)
+		love.graphics.print(hud_line_1, love.mouse.getX()+1, love.mouse.getY()+1)
+		love.graphics.print(hud_line_1, love.mouse.getX()-1, love.mouse.getY()-1)
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.print(hud_line_1, love.mouse.getX(), love.mouse.getY())
+		-- line 2
+		love.graphics.setColor(0, 0, 0, 255)
+		love.graphics.print(hud_line_2, love.mouse.getX()+1, love.mouse.getY()+32+1)
+		love.graphics.print(hud_line_2, love.mouse.getX()-1, love.mouse.getY()+32-1)
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.print(hud_line_2, love.mouse.getX(), love.mouse.getY()+32)
 	end
 end
 
@@ -121,9 +112,12 @@ function love.quit()
 	fh:close()
 end
 
--- takes x,y coordinates and /r/place colour value
+-- takes x,y coordinates and /r/place colour value and adds them to the canvas
 function putPixel(x, y, c)
-	love.graphics.setColor(rgb_red[c], rgb_green[c], rgb_blue[c])
+	love.graphics.setCanvas(canvas)
+    love.graphics.setBlendMode("alpha")
+	love.graphics.setColor(rgb_red[c], rgb_green[c], rgb_blue[c], 255)
 	love.graphics.points(x, y)
+	love.graphics.setCanvas()
 end
 
